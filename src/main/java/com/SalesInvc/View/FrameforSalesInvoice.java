8c23fe1151c0390b8508da67952b7de1d1a57ff1
ziadelkersh <ziadelersh@gmail.com>
2022-11-20 14:23:12 +0200

    third commit

--- src/main/java/com/SalesInvc/View/FrameforSalesInvoice.java
+++ src/main/java/com/SalesInvc/View/FrameforSalesInvoice.java
@@ -55,8 +55,8 @@ public class FrameforSalesInvoice extends javax.swing.JFrame {
         ItemCreation.addActionListener(InvcContrl);
         ItemDeletion = new javax.swing.JButton();
         ItemDeletion.addActionListener(InvcContrl);
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         MenuItemLoadFile = new javax.swing.JMenuItem();
@@ -133,16 +133,11 @@ public class FrameforSalesInvoice extends javax.swing.JFrame {
         ItemDeletion.setText("Cancel");
         ItemDeletion.setToolTipText("");
 
-        jTextField1.setText("Invoices Table");
-        jTextField1.setToolTipText("");
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
+        jLabel5.setText("Invoices Items");
+        jLabel5.setToolTipText("");
 
-        jTextField2.setText("Invoices Items");
-        jTextField2.setToolTipText("");
+        jLabel6.setText("Invoices Table");
+        jLabel6.setToolTipText("");
 
         jMenu1.setText("File");
 
@@ -174,7 +169,7 @@ public class FrameforSalesInvoice extends javax.swing.JFrame {
                         .addComponent(InvoiceDeletion))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jLabel6)))
                 .addGap(12, 12, 12)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 310, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -194,7 +189,7 @@ public class FrameforSalesInvoice extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(InvoiceLabelDate, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jLabel1)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel5))
                 .addContainerGap(44, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -213,9 +208,9 @@ public class FrameforSalesInvoice extends javax.swing.JFrame {
                         .addGap(18, 18, 18))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1))
-                        .addGap(2, 2, 2)))
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel6))
+                        .addGap(5, 5, 5)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -231,9 +226,9 @@ public class FrameforSalesInvoice extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel4)
+                                .addGap(11, 11, 11)
+                                .addComponent(jLabel5)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(8, 8, 8)
                                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(SumTotalInvoice)))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
@@ -244,7 +239,7 @@ public class FrameforSalesInvoice extends javax.swing.JFrame {
                         .addComponent(InvoiceDeletion))
                     .addComponent(ItemDeletion)
                     .addComponent(ItemCreation))
-                .addContainerGap(454, Short.MAX_VALUE))
+                .addContainerGap(460, Short.MAX_VALUE))
         );
 
         ItemCreation.getAccessibleContext().setAccessibleDescription("");
@@ -263,10 +258,6 @@ public class FrameforSalesInvoice extends javax.swing.JFrame {
     private void InvoiceDeletionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InvoiceDeletionActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_InvoiceDeletionActionPerformed
-
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -320,12 +311,12 @@ public class FrameforSalesInvoice extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
  private ArrayList<InvoiceClass> invoices;
  private InvoiceController InvcContrl = new InvoiceController(this);
